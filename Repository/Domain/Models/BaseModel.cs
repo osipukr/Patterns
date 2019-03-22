2027a5c13ec963d9be0040ee378fbfda950b5904
FredSt0ne <fredstone624@gmail.com>
2019-03-22 17:39:47 +0300

    add models

---
+++ Repository/Domain/Models/BaseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Repository.Domain.Models
+{
+    public abstract class BaseModel
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ Repository/Domain/Models/Photo.cs
@@ -0,0 +1,11 @@
+﻿namespace Repository.Domain.Models
+{
+    public class Photo : BaseModel
+    {
+        public string AlbumId { get; set; }
+        public Album Album { get; set; }
+
+        public string ImageId { get; set; }
+        public Image Image { get; set; }
+    }
+}

78960b3fc0c6f912ed319bccec5cdc469ec1dd2f
FredSt0ne <fredstone624@gmail.com>
2019-03-23 13:34:23 +0300

    add testing program

--- Repository/Program.cs
+++ Repository/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Repository.Domain.Models;
+using Repository.Persistence.Contexts;
+using Repository.Persistence.Repositories;
+using System;
+using System.Linq;
 
 namespace Repository
 {
@@ -6,7 +10,50 @@ namespace Repository
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var uow = new UnitOfWork(new AppDbContext()))
+            {
+                Console.WriteLine("Albums:");
+                uow.Albums.GetAll().ToList().ForEach(album =>
+                {
+                    PrintAlbum(album);
+                });
+
+                Console.WriteLine("Photos:");
+                uow.Photos.GetAll().ToList().ForEach(photo =>
+                {
+                    PrintPhoto(photo);
+                });
+
+                Console.WriteLine("Images:");
+                uow.Images.GetAll().ToList().ForEach(image =>
+                {
+                    PrintImage(image);
+                });
+            }
+
+            Console.ReadKey();
         }
+
+        #region PrintHelper
+        private static void PrintAlbum(Album album)
+        {
+            Console.WriteLine($"Album | Id - {album.Id}");
+            Console.WriteLine($"Album | Name - {album.Name}");
+            Console.WriteLine($"Album | Description - {album.Description}\n");
+        }
+
+        private static void PrintPhoto(Photo photo)
+        {
+            Console.WriteLine($"Photo | Id - {photo.Id}");
+            Console.WriteLine($"Photo | ImageId - {photo.ImageId}\n");
+        }
+
+        private static void PrintImage(Image image)
+        {
+            Console.WriteLine($"Image | Id - {image.Id}");
+            Console.WriteLine($"Image | Url - {image.Url}");
+            Console.WriteLine($"Image | PhotoId - {image.PhotoId}\n");
+        }
+        #endregion
     }
 }
